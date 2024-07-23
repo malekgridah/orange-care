202e75e131807fa68881be488f101c83576aa914
malek.gridah <malek.gridah@billcom-consulting.com>
2024-07-23 15:51:48 +0100

    Refactoring config binding using orange care config server

--- orange-care-financials/src/main/java/com/billcom/financials/FinancialsApplication.java
+++ orange-care-financials/src/main/java/com/billcom/financials/FinancialsApplication.java
@@ -3,6 +3,7 @@ package com.billcom.financials;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.client.ConfigServicePropertySourceLocator;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,4 +12,5 @@ public class FinancialsApplication {
     public static void main(String[] args) {
         SpringApplication.run(FinancialsApplication.class);
     }
+
 }
