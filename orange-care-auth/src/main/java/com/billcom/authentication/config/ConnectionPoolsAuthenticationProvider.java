a8a805f2264a24338bcfc6b21e5077c66146266e
malek.gridah <malek.gridah@billcom-consulting.com>
2024-07-25 23:33:19 +0100

    Refactoring the SOI connection via auth server as an authentication provider

--- orange-care-auth/src/main/java/com/billcom/authentication/config/ConnectionPoolsAuthenticationProvider.java
+++ orange-care-auth/src/main/java/com/billcom/authentication/config/ConnectionPoolsAuthenticationProvider.java
@@ -3,24 +3,17 @@ package com.billcom.authentication.config;
 import com.lhs.ccb.cfw.cda.utility.GlobalUserProperties;
 import com.lhs.ccb.cfw.cda.utility.UserPropertiesFacade;
 import com.lhs.ccb.cfw.wcs.security.JaasAuthenticationProvider;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Log4j2
-@Component
-public class SampleAuthManager implements AuthenticationProvider {
-
-
+public class ConnectionPoolsAuthenticationProvider implements AuthenticationProvider {
     static final List<GrantedAuthority> AUTHORITIES = new ArrayList<>();
 
     @Override
@@ -37,13 +30,13 @@ public class SampleAuthManager implements AuthenticationProvider {
             return new UsernamePasswordAuthenticationToken(authentication.getName(),
                     authentication.getCredentials(), AUTHORITIES);
         } catch (Exception e) {
-            log.error(e.getMessage());
             throw new BadCredentialsException("Invalid Username or Password");
         }
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
-        return false;
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
     }
+
 }
